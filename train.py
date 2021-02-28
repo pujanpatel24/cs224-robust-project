9f23799a84b70a17d0fc1092ade93bf3cfd57aa1
Max Pike <mpike27@stanford.edu>
2021-02-27 18:07:10 -0600

    nui

--- train.py
+++ train.py
@@ -31,7 +31,9 @@ def augment_by_chunk(translator, lst):
     aug = ""
     chunks = [string[i:i+step] for i in range(0, len(string), step)]
     for chunk in chunks:
+        print(chunk)
         aug += backTranslate(translator, chunk, 'es')
+        input(aug)
     print(f"Length of augmented string is {len(aug)}")
     return_list = aug.strip('][').split(', ')
     print(f"Length of augmented list is: {len(return_list)}")
