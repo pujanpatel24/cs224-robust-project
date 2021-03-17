dc58eaf42b5fd750ef63aec61c25fd4df4b4ba19
pujanpatel24 <pujanspatel@gmail.com>
2021-03-17 17:48:34 -0500

    Andy dumb

--- grid_search.py
+++ grid_search.py
@@ -2,10 +2,9 @@
 from home directory, run python scripts/template_run.py 
 use flag --print_only to print formatted commands, but not run them
 """
-import os, argparse
+
 from subprocess import call
-from sklearn.model_selection import ParameterGrid
-from datetime import date
+
 
 
 # key is the argument name expected by main.py
@@ -23,7 +22,7 @@ def main():
     for bs in GRID_SEARCH['batch-size']:
         for lr in GRID_SEARCH['lr']:
             for epochs in GRID_SEARCH['num-epochs']:
-                for epochs in GRID_SEARCH['weight-decay']:
+                for wd in GRID_SEARCH['weight-decay']:
                     model_name = "grid_search_bs_{}_lr_{}_wd_{}".format(bs, lr, wd)
                     cmd = "python train.py --run-name {} --eval-every 50 --model-path {} ".format(model_name, "save/baseline-early-stop-01")
                     cmd += "--batch-size {} ".format(bs)
