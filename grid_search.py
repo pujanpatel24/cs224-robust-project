aca06fdfafb97b725df89f14e2d85252ec4c0ec6
andrewNhwang <44788850+andrewNhwang@users.noreply.github.com>
2021-03-17 12:41:25 -0700

    Update grid_search.py

--- grid_search.py
+++ grid_search.py
@@ -11,39 +11,35 @@ from datetime import date
 # key is the argument name expected by main.py
 # value should be a list of options
 GRID_SEARCH = {
-    'batch-size': [2,4,8,16],
-    'lr': [5e-5,1e-4,1e-3, 1e-2],
-    'num-epochs': [40],
-    'num-layers': [0,1,2]
+    'batch-size': [8,16],
+    'lr': [5e-5, 1e-5, 5e-6, 1e-6, 5e-7],
+    'num-epochs': [7],
+    'patience': 3,
+    'weight-decay:, [1e-2, 1e-1, 0.5], 
 }
 
 
 def main():
-    for bs in [2,4,8,16]:
-        for lr in [5e-5,1e-4,1e-3, 1e-2]:
-            for epochs in [40]:
-                for layers in [-1,0,1,2]:
-                    for backtranslation in [False, True]:
-                        for synonym in [False, True]:
-                            model_name = "grid_search_bs_{}_lr_{}_num_layers_{}_bt_{}_syn_{}".format(bs, lr, layers, backtranslation, synonym)
-                            cmd = "python train.py --run-name {} --eval-every 150 --model-path {} ".format(model_name, "save/baseline-early-stop-01")
-                            cmd += "--batch-size {} ".format(bs)
-                            cmd += "--lr {} ".format(lr)
-                            cmd += "--num-epochs {} ".format(epochs)
-                            if layers == -1:
-                                cmd += "--do-train "
-                            else:
-                                cmd += "--do-finetune "
-                                cmd += "--num-layers {} ".format(layers)
-                            if backtranslation:
-                                cmd += "--backtranslation "
-                            if synonym:
-                                cmd += "--synonym "
+    for bs in GRID_SEARCH['batch-size']:
+        for lr in GRID_SEARCH['lr']:
+            for epochs in GRID_SEARCH['num-epochs']:
+                for epochs in GRID_SEARCH['weight-decay']:
+                    model_name = "grid_search_bs_{}_lr_{}_wd_{}".format(bs, lr, wd)
+                    cmd = "python train.py --run-name {} --eval-every 50 --model-path {} ".format(model_name, "save/baseline-early-stop-01")
+                    cmd += "--batch-size {} ".format(bs)
+                    cmd += "--lr {} ".format(lr)
+                    cmd += "--num-epochs {} ".format(epochs)
+                    cmd += "--weight-decay {} ".format(wd)
+                    cmd += "--do-finetune "
+                    cmd += "--backtranslation "
+                    cmd += "--synonym "
+                    cmd += "--visualize-predictions "
+                    cmd += "--recompute-features "
 
-                            output = call(cmd, shell=True)
-                            f = open("results.txt", "a")
-                            f.write("Results for model_name {} is {}".format(model_name, output))
-                            f.close()
+                    output = call(cmd, shell=True)
+                    f = open("results.txt", "a")
+                    f.write("Results for model_name {} is {}".format(model_name, output))
+                    f.close()
 
 if __name__ == '__main__':
     main()
