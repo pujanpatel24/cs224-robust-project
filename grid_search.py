08f47c4d225b07a35e129c571c995ba1d3865bcf
andrewNhwang <44788850+andrewNhwang@users.noreply.github.com>
2021-03-18 13:56:48 -0700

    Update grid_search.py

--- grid_search.py
+++ grid_search.py
@@ -10,11 +10,11 @@ from subprocess import call
 # key is the argument name expected by main.py
 # value should be a list of options
 GRID_SEARCH = {
-    'batch-size': [16, 8],
-    'lr': [ 1e-6, 5e-7],
+    'batch-size': [16],
+    'lr': [ 1e-6],
     'num-epochs': [7],
     'patience': 4,
-    'weight-decay': [1e-2, 1e-1, 0.5],
+    'weight-decay': [0.5],
 }
 
 
@@ -23,17 +23,21 @@ def main():
         for lr in GRID_SEARCH['lr']:
             for epochs in GRID_SEARCH['num-epochs']:
                 for wd in GRID_SEARCH['weight-decay']:
-                    model_name = "grid_search_bs_{}_lr_{}_wd_{}".format(bs, lr, wd)
-                    cmd = "python train.py --run-name {} --eval-every 50 --model-path {} ".format(model_name, "save/baseline-early-stop-01/checkpoint")
-                    cmd += "--batch-size {} ".format(bs)
-                    cmd += "--lr {} ".format(lr)
-                    cmd += "--num-epochs {} ".format(epochs)
-                    cmd += "--weight-decay {} ".format(wd)
-                    cmd += "--do-finetune "
-                    cmd += "--backtranslation "
-                    cmd += "--synonym "
-                    cmd += "--visualize-predictions "
-                    cmd += "--recompute-features "
+                    for syn in [0, 1]:
+                        for bktranslation in [0,1]:
+                            model_name = "DATA_grid_search_bs_{}_lr_{}_wd_{}".format(bs, lr, wd)
+                            cmd = "python train.py --run-name {} --eval-every 50 --model-path {} ".format(model_name, "save/baseline-early-stop-01/checkpoint")
+                            cmd += "--batch-size {} ".format(bs)
+                            cmd += "--lr {} ".format(lr)
+                            cmd += "--num-epochs {} ".format(epochs)
+                            cmd += "--weight-decay {} ".format(wd)
+                            cmd += "--do-finetune "
+                            if bktranslation:
+                                cmd += "--backtranslation "
+                            if syn:
+                                cmd += "--synonym "
+                            cmd += "--visualize-predictions "
+                            cmd += "--recompute-features "
 
                     output = call(cmd, shell=True)
                     f = open("results.txt", "a")
